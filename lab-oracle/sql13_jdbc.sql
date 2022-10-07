188d07f2f0fa23750ab8b4dbd18fa4856300cd2c
young <0loveyo0602@gmail.com>
2022-10-07 14:36:02 +0900

    create, insert, update

--- lab-oracle/sql13_jdbc.sql
+++ lab-oracle/sql13_jdbc.sql
@@ -10,14 +10,14 @@
 --  MODIFIED_DATE: 블로그 글 수정 시간.  timestamp. 기본값= 시스템 현재 시간.
 
 CREATE TABLE BLOGS (
-    BLOGS_NO        number(4), 
+    BLOG_NO         number(4), 
     TITLE           varchar2(100 char),
     CONTENT         varchar2(1000 char),
     AUTHOR          varchar2(100 char),
     CREATED_DATE    TIMESTAMP DEFAULT sysdate,
     MODIFIED_DATE   TIMESTAMP DEFAULT sysdate,
     
-    CONSTRAINT BLOGS_NO_PK PRIMARY KEY(BLOGS_NO),
+    CONSTRAINT BLOGS_NO_PK PRIMARY KEY(BLOG_NO),
     CONSTRAINT BLOGS_TITLE_NN check(TITLE is not null),
     CONSTRAINT BLOGS_CONTENT_NN check(CONTENT is not null),
     CONSTRAINT BLOGS_AUTHOR_NN check(AUTHOR is not null)
@@ -34,11 +34,23 @@ VALUES ('테스트', '새 블로그 작성 테스트', 'admin');
 SELECT * FROM blogs;
 
 INSERT INTO blogs (TITLE , CONTENT, AUTHOR)
-VALUES ('테스트2', '새 블로그 작성 테스트', 'admin');
+VALUES ('테스트4', '작성 테스트', 'guest');
 
 -- 글번호 2번 글의 제목과 내용을 수정하고 수정 날짜 컬럼을 시스템 현재 시간으로 업데이트
 UPDATE blogs 
-    SET title = '테스트22', content = ' 수정 테스트', modified_date = sysdate 
-    WHERE blogs_no = 2;
+    SET title = '테스트2', content = '수정 테스트', modified_date = sysdate 
+    WHERE blog_no = 2;
 
 commit;
+
+
+
+
+
+
+
+
+
+
+
+
