93eac6a340f5f2fc6d3a8db44dd3ee9a7f7615b4
young <0loveyo0602@gmail.com>
2022-11-22 18:21:02 +0900

    Spring Boot

--- lab-web/spring03/src/main/java/com/example/spring03/web/HomeController.java
+++ lab-web/spring03/src/main/java/com/example/spring03/web/HomeController.java
@@ -1,21 +1,32 @@
 package com.example.spring03.web;
 
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import com.example.spring03.domain.Post;
+import com.example.spring03.service.PostService;
+
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
+@RequiredArgsConstructor
 @Controller // 스프링 컨트롤러 컴포넌트
 public class HomeController {
-
+    
+    private final PostService postService; // 생성자에 의해서 의존성 주입
+    
     @GetMapping("/") // 요청 URL/방식 매핑
     public String home(Model model) {
         log.info("home()");
         
-        model.addAttribute("msg", "Hello, 스프링 부트 프로젝트!");
+        List<Post> list = postService.read(); // DB에서 포스트 목록 전체 검색
+        model.addAttribute("list", list); // 뷰에 전달하는 모델 데이터.
         
-        return "home";  // View 이름 -> src/main/resources/templates/파일이름.html 생략
+        return "/post/list";  
+        // View 이름 -> src/main/resources/templates/파일이름.html 생략
     }
 }
---
+++ lab-web/spring03/src/main/java/com/example/spring03/web/PostController.java
@@ -0,0 +1,42 @@
+package com.example.spring03.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import com.example.spring03.domain.Post;
+import com.example.spring03.dto.PostCreateDto;
+import com.example.spring03.service.PostService;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@RequiredArgsConstructor
+@Controller
+@RequestMapping("/post")
+public class PostController {
+    
+    private final PostService postService;
+    
+    @GetMapping("/create")
+    public void create() {
+        log.info("create()");
+        // 컨트롤러 메서드의 리턴 타입이 void 인 경우 뷰의 이름은 요청 주소와 같음
+    }
+    
+    @PostMapping("/create") // Post 방식의 /post/create 요청을 처리하는 메서드.
+    public String create(PostCreateDto dto, RedirectAttributes attrs) {
+        log.info("create(dto={})" , dto);
+        
+        Post entity = postService.create(dto);
+        
+        // 작성된 포스트의 번호(id)를 redirect 되는 페이지로 전달
+        attrs.addFlashAttribute("createdId", entity.getId());
+        
+        //PRG (Post-Redirect-Get) 패턴
+        return "redirect:/";
+    }
+}
